782df5ffa6fb2bfa232255a56211d4fed480498f
CaptainSumo <b.hillier@picturae.com>
2018-02-09 19:46:23 +0100

    Mod rewrite added

---
+++ application/controllers/data.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bhillier
+ * Date: 09/02/2018
+ * Time: 19:34
+ */
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Data extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+	print "gi";
+	return;
+		$this->load->view('welcome_message');
+	}
+}
