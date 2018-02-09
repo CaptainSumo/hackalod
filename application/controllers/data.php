53fd99f7d3545724152235490ce8003b9e910259
CaptainSumo <b.hillier@picturae.com>
2018-02-09 20:32:57 +0100

    Kunstromingen

--- application/controllers/data.php
+++ application/controllers/data.php
@@ -11,25 +11,95 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Data extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
+    /**
+     * @param string $query Query to run against Wikidata
+     */
+    private function queryWikiData($query){
+
+
+        $endpointUrl = 'https://query.wikidata.org/sparql';
+
+        // create curl resource
+        $ch = curl_init();
+
+        // set url
+        curl_setopt($ch, CURLOPT_URL,  $endpointUrl . '?query=' . urlencode( $query ) );
+
+        //return the transfer as a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Accept: application/sparql-results+json'
+        ));
+
+        // $output contains the output string
+        $output = curl_exec($ch);
+
+        // close curl resource to free up system resources
+        curl_close($ch);
+
+        return json_decode($output, true);
+
+    }
+
+
+    /*
+     * Gets all art movements.
+     * We use this when there is no artist because the query will be faster
+     *
+     *
+     *
+     */
+    private function getAllMovements(){
+        $query = <<<QUERY_ALL_MOVEMENTS
+SELECT * WHERE {
+  SELECT *   WHERE {
+  ?s wdt:P135 ?movement.    #Subject in movement
+  ?s wdt:P106 wd:Q1028181.  #Subject is painter
+  ?s wdt:P31 wd:Q5.         #Subject is person
+  ?s rdfs:label ?person.
+   filter (lang(?person) = "nl").
+
+  {
+SELECT ?movement ?label WHERE {
+     
+   ?movement wdt:P31 wd:Q968159  .   #Instance of movement
+   ?movement rdfs:label ?label.
+   filter (lang(?label) = "nl").
+}
+}
+ 
+}
+  
+  
+}
+
+QUERY_ALL_MOVEMENTS;
+
+        $allMovements = $this->queryWikiData($query);
+
+        $resultData = $allMovements['results']['bindings'];
+
+        $dataOut = array();
+
+        foreach ($resultData as $row){
+            $movement = $row['label']['value'];
+            if(!isset($dataOut[$movement])){
+                $dataOut[$movement] = 0;
+            }
+            $dataOut[$movement]++;
+        }
+
+        return array('queries' => (array)$query, 'data' =>$dataOut);
+
+    }
+
+
+	//Gets all movements for this artist
+	public function kunststroming($artist = '')
 	{
-	print "gi";
-	return;
-		$this->load->view('welcome_message');
+	    $returnData = $this->getAllMovements();
+	    print json_encode($returnData);
+	    return;
 	}
 }
