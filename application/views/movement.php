e044f815d475bd74c8d2d6843b8b0f472db6651f
CaptainSumo <b.hillier@picturae.com>
2018-02-09 22:33:43 +0100

    Working TagCloud

---
+++ application/views/movement.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="./assets/css/site.css">
+</head>
+<body>
+<h1>Movement <?php echo $name ?></h1>
+
+
+</body>
+</html>
